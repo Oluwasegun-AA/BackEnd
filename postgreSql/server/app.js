305bc19e2e0045cef8d7e19929b9e51895034067
shegsteham <oluwasegunadepoju@gmail.com>
2019-10-15 16:04:06 +0200

    ft(setup): setup server

--- postgreSql/server/app.js
+++ postgreSql/server/app.js
@@ -1,8 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+import router from '../routes';
 import { connectionMessage } from '../helpers';
+import { catchAllError }  from '../helpers';
 
 dotenv.config();
 const app = express();
+const { PORT } = process.env;
 
-app.listen(port, connectionMessage(port));
+app.use(logger('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/v1', router);
+
+catchAllError(app)
+
+app.listen(PORT, connectionMessage(PORT));
