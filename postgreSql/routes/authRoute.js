2b6d04d5a30e3cf16b8ec170a0b8705ebf27847c
shegsteham <oluwasegunadepoju@gmail.com>
2019-10-21 11:46:16 +0200

    ft(feeds):implement feeds route

--- postgreSql/routes/authRoute.js
+++ postgreSql/routes/authRoute.js
@@ -1,15 +1,15 @@
 import express from 'express';
 
 import { catchAllError } from '../helpers';
-import Auth from '../controllers/AuthController';
+import { AuthController } from '../controllers';
 import {
   validateSignupData,
   validateLoginData,
-  checkUserExist
+  checkUserExist,
 } from '../middlewares';
 
 const auth = express.Router();
-const { login, signup } = Auth;
+const { login, signup } = AuthController;
 
 auth.post('/login', validateLoginData, checkUserExist, login);
 auth.post('/signup', validateSignupData, checkUserExist, signup);
